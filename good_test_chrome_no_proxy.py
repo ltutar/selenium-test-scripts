27aa5b6ad04db786639075bbb3312dff2dc88544
Levent Tutar <ltutar@xebia.com>
2020-03-11 11:27:07 +0100

    Create good_test_chrome_no_proxy.py

---
+++ good_test_chrome_no_proxy.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+#https://peter.sh/experiments/chromium-command-line-switches/
+chrome_options.add_argument('--no-sandbox') # Bypass OS security model
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--headless") # Runs Chrome in headless mode.
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+
+time.sleep(4)
+
+print(driver.page_source.encode("utf-8"))
+
+assert "No results found." not in driver.page_source
+
+print ("Headless Chrome Initialized on Linux OS")
+
+driver.close()
